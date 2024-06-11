d9b94307bda933006750692e37ba1af6d755b418
Vitor Melo <89869031+VitorMeloDev@users.noreply.github.com>
2024-06-11 17:06:02 -0300

    Item size math

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 //fiz esse codigo, mas na tela não mostra corretamente, somente uma pequena tarja preta, imagino que o problema esteja na hora de passar o tamanho do item
 
 const displayContainer = document.querySelector(".container");
-let rows = 3;
-const itemSize = displayContainer.clientWidth / (rows * rows);
+let rows = 16;
 
 function CreateElementsOnDisplay()
 {
     for (let i = 0; i < rows*rows; i++) {
         const item = document.createElement('div');
         item.classList.add('flex-item');
-        item.style.width = '318px';
-        item.style.height = '318px';
+        item.style.width = ItemSizeWidthHeight();
+        item.style.height = ItemSizeWidthHeight();
     
         displayContainer.appendChild(item);
     }
+}
+
+function ItemSizeWidthHeight()
+{
+    const size = displayContainer.clientWidth / rows - 3;
+    return `${size}px`;
 }
 
 CreateElementsOnDisplay();
